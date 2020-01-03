9c0b68208befe9434dfef3bfdebd4aa0a7199c2c
bentis <ben@tisdall.org.uk>
2020-01-03 21:37:49 +0100

    CVSIGNORE (used by rsync)

--- sh/exports.sh
+++ sh/exports.sh
@@ -8,4 +8,5 @@ export LANG=en_GB.UTF-8
 export EDITOR=vim
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export WORKON_HOME=/Users/bentis/.local/share/virtualenvs
+export CVSIGNORE="._* .Spotlight* .DS_Store .fseventsd"
 [ -f /usr/libexec/java_home ] && export JAVA_HOME="$(/usr/libexec/java_home)"
